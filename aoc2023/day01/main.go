ae9d7dc92c93ae12a0b7c0996475bdac0b177b68
Mike Helmick <mhelmick@roblox.com>
2023-11-30 21:38:59 -0800

    day1 solution

--- aoc2023/day01/main.go
+++ aoc2023/day01/main.go
@@ -5,20 +5,99 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
+
+var (
+	partOne = map[string]string{
+		"1": "1",
+		"2": "2",
+		"3": "3",
+		"4": "4",
+		"5": "5",
+		"6": "6",
+		"7": "7",
+		"8": "8",
+		"9": "9",
+	}
+
+	partTwo = map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+		"1":     "1",
+		"2":     "2",
+		"3":     "3",
+		"4":     "4",
+		"5":     "5",
+		"6":     "6",
+		"7":     "7",
+		"8":     "8",
+		"9":     "9",
+	}
+)
+
+func firstDigit(s string, m map[string]string) string {
+	pos := len(s) + 1
+	val := ""
+	for k, v := range m {
+		if p := strings.Index(s, k); p >= 0 && p < pos {
+			pos = p
+			val = v
+		}
+	}
+	return val
+}
+
+func lastDigit(s string, m map[string]string) string {
+	pos := -1
+	val := ""
+	for k, v := range m {
+		if p := strings.LastIndex(s, k); p >= 0 && p > pos {
+			pos = p
+			val = v
+		}
+	}
+	return val
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
+	part1 := 0
+	part2 := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line == "" {
 			continue
 		}
 
-		fmt.Println(line)
+		p1 := firstDigit(line, partOne) + lastDigit(line, partOne)
+		p2 := firstDigit(line, partTwo) + lastDigit(line, partTwo)
+
+		v, err := strconv.Atoi(p1)
+		if err == nil {
+			part1 += v
+		}
+
+		//fmt.Printf("%v -> %v\n", line, p2)
+		p2v, err := strconv.Atoi(p2)
+		if err != nil {
+			panic(fmt.Errorf("unable to parse %q input %q, err: %v", p2, line, err))
+		}
+		part2 += p2v
 	}
 	if err := scanner.Err(); err != nil {
 		log.Println(err)
 	}
+
+	fmt.Printf("part1: %v\n", part1)
+	fmt.Printf("part2: %v\n", part2)
 }
