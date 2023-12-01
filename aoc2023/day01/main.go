987193572d24837cff71e319ffda1045f6cf9559
Mike Helmick <mhelmick@roblox.com>
2023-12-01 09:02:57 -0800

    better solution for day 1

--- aoc2023/day01/main.go
+++ aoc2023/day01/main.go
@@ -3,69 +3,42 @@ package main
 import (
 	"bufio"
 	"os"
-	"strconv"
+	"sort"
 	"strings"
 
 	"github.com/mikehelmick/adventofcode/aoc2023/pkg/logging"
 )
 
 var (
-	partOne = map[string]string{
-		"1": "1",
-		"2": "2",
-		"3": "3",
-		"4": "4",
-		"5": "5",
-		"6": "6",
-		"7": "7",
-		"8": "8",
-		"9": "9",
-	}
-
-	partTwo = map[string]string{
-		"one":   "1",
-		"two":   "2",
-		"three": "3",
-		"four":  "4",
-		"five":  "5",
-		"six":   "6",
-		"seven": "7",
-		"eight": "8",
-		"nine":  "9",
-		"1":     "1",
-		"2":     "2",
-		"3":     "3",
-		"4":     "4",
-		"5":     "5",
-		"6":     "6",
-		"7":     "7",
-		"8":     "8",
-		"9":     "9",
-	}
+	partOne = map[string]int{"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9}
+	partTwo = map[string]int{
+		"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9,
+		"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9}
 )
 
-func firstDigit(s string, m map[string]string) string {
-	pos := len(s) + 1
-	val := ""
-	for k, v := range m {
-		if p := strings.Index(s, k); p >= 0 && p < pos {
-			pos = p
-			val = v
-		}
-	}
-	return val
+type Digit struct {
+	Pos   int
+	Value int
 }
 
-func lastDigit(s string, m map[string]string) string {
-	pos := -1
-	val := ""
+type Index []Digit
+
+func indexDigits(s string, m map[string]int) Index {
+	rtn := make(Index, 0, 2)
 	for k, v := range m {
-		if p := strings.LastIndex(s, k); p >= 0 && p > pos {
-			pos = p
-			val = v
+		r := s
+		firstPos := strings.Index(r, k)
+		if firstPos >= 0 {
+			rtn = append(rtn, Digit{firstPos, v})
+		}
+		if lastPos := strings.LastIndex(r, k); lastPos >= 0 && lastPos != firstPos {
+			rtn = append(rtn, Digit{lastPos, v})
 		}
 	}
-	return val
+	sort.Slice(rtn, func(i, j int) bool {
+		return rtn[i].Pos <= rtn[j].Pos
+	})
+	return rtn
 }
 
 func main() {
@@ -80,20 +53,14 @@ func main() {
 			continue
 		}
 
-		p1 := firstDigit(line, partOne) + lastDigit(line, partOne)
-		p2 := firstDigit(line, partTwo) + lastDigit(line, partTwo)
-
-		v, err := strconv.Atoi(p1)
-		if err == nil {
-			part1 += v
+		p1idx := indexDigits(line, partOne)
+		if len(p1idx) > 0 { // some examples in the p2 example don't parse
+			part1 += (p1idx[0].Value*10 + p1idx[len(p1idx)-1].Value)
 		}
 
-		log.Debugf("%v -> %v", line, p2)
-		p2v, err := strconv.Atoi(p2)
-		if err != nil {
-			log.Panicf("unable to parse %q input %q, err: %v", p2, line, err)
-		}
-		part2 += p2v
+		p2idx := indexDigits(line, partTwo)
+		log.Debugf("%v -> %+v", line, p2idx)
+		part2 += (p2idx[0].Value*10 + p2idx[len(p2idx)-1].Value)
 	}
 	if err := scanner.Err(); err != nil {
 		log.Errorw("io error", "error", err)
