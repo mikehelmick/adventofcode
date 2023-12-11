6c911f8c3d4ac1dc7f7e0b0c98761e0dcac8955c
Mike Helmick <mike.helmick@gmail.com>
2023-12-10 21:45:08 -0800

    day11

--- aoc2023/day11/main.go
+++ aoc2023/day11/main.go
@@ -3,12 +3,88 @@ package main
 import (
 	"bufio"
 	"context"
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/mikehelmick/adventofcode/pkg/logging"
+	"github.com/mikehelmick/adventofcode/pkg/twod"
+	"gonum.org/v1/gonum/stat/combin"
 )
 
-type Data struct {
+type Grid [][]string
+
+func (g Grid) String() string {
+	b := strings.Builder{}
+	for _, r := range g {
+		b.WriteString(fmt.Sprintf("%+v\n", r))
+	}
+	return b.String()
+}
+
+func (g Grid) Points() []*twod.Pos {
+	points := make([]*twod.Pos, 0)
+	for r, row := range g {
+		for c, char := range row {
+			if char == "#" {
+				points = append(points, twod.NewPos(r, c))
+			}
+		}
+	}
+	return points
+}
+
+func isEmpty(r []string) bool {
+	for _, s := range r {
+		if s == "#" {
+			return false
+		}
+	}
+	return true
+}
+
+func (g Grid) EmptyCols() []int {
+	emptyCols := make([]int, 0)
+	for col := 0; col < len(g[0]); col++ {
+		isEmpty := true
+		for row := 0; row < len(g) && isEmpty; row++ {
+			isEmpty = g[row][col] != "#"
+		}
+		if isEmpty {
+			emptyCols = append(emptyCols, col)
+		}
+	}
+	return emptyCols
+}
+
+func (g Grid) EmptyRows() []int {
+	r := make([]int, 0)
+	for rN, row := range g {
+		if isEmpty(row) {
+			r = append(r, rN)
+		}
+	}
+	return r
+}
+
+func expand(factor int, points []*twod.Pos, emptyRows, emptyCols []int) []*twod.Pos {
+	newPoints := make([]*twod.Pos, 0, len(points))
+	for _, p := range points {
+		addR := 0
+		addC := 0
+		for _, er := range emptyRows {
+			if p.Row > er {
+				addR += factor
+			}
+		}
+		for _, ec := range emptyCols {
+			if p.Col > ec {
+				addC += factor
+			}
+		}
+		newPoints = append(newPoints, twod.NewPos(p.Row+addR, p.Col+addC))
+	}
+	return newPoints
 }
 
 func main() {
@@ -17,13 +93,43 @@ func main() {
 
 	scanner := bufio.NewScanner(os.Stdin)
 
+	grid := make(Grid, 0)
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line == "" {
 			continue
 		}
-		log.Debugw("parsed line", "line", line)
+
+		chars := strings.Split(line, "")
+		grid = append(grid, chars)
 	}
+	log.Debugw("loaded", "grid", grid)
+
+	points := grid.Points()
+	log.Debugw("found points", "points", points)
+
+	emptyRows := grid.EmptyRows()
+	emptyCols := grid.EmptyCols()
+
+	part1Points := expand(1, points, emptyRows, emptyCols)
+	log.Debugw("found points", "points", part1Points)
+
+	pairs := combin.Combinations(len(part1Points), 2)
+	log.Debug("calculated pairs", "pairs", pairs)
+
+	allDist := 0
+	for _, pair := range pairs {
+		allDist += part1Points[pair[0]].Dist(part1Points[pair[1]])
+	}
+	log.Infow("answer", "part1", allDist)
+
+	part2Points := expand(999999, points, emptyRows, emptyCols)
+	log.Debugw("found points", "part2points", part2Points)
+	p2answer := int64(0)
+	for _, pair := range pairs {
+		p2answer += int64(part2Points[pair[0]].Dist(part2Points[pair[1]]))
+	}
+	log.Infow("answer", "part2", p2answer)
 
 	if err := scanner.Err(); err != nil {
 		log.Errorw("read error", "err", err)
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.21.4
 require (
 	github.com/mikehelmick/go-functional v0.3.0
 	go.uber.org/zap v1.26.0
+	gonum.org/v1/gonum v0.14.0
 )
 
 require go.uber.org/multierr v1.10.0 // indirect
